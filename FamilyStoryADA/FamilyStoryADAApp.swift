e9593ad05b24ea10bf494d339ba24fe0c60b589f
NJuGau <nathanael.gauthama@binus.ac.id>
2024-09-26 14:45:50 +0700

    Fix Orientation and display caused by YML

--- FamilyStoryADA/FamilyStoryADAApp.swift
+++ FamilyStoryADA/FamilyStoryADAApp.swift
@@ -11,8 +11,7 @@ import SwiftUI
 struct FamilyStoryADAApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
-                .ignoresSafeArea()
+            ExampleView()
         }
     }
 }
--- FamilyStoryADA/Presentation/Epic1-SocialStory/View/ExampleView.swift
+++ FamilyStoryADA/Presentation/Epic1-SocialStory/View/ExampleView.swift
@@ -10,7 +10,9 @@ import SwiftUI
 struct ExampleView: View {
     @ObservedObject var model: ExampleViewModel = ExampleViewModel(id: UUID(uuidString: "37bff686-7d09-4e53-aa90-fb465da131b5")!)
     var body: some View {
-        Text(model.exampleData.first?.name ?? "")
+        GeometryReader { geometry in
+            Text(model.exampleData.first?.name ?? "")
+        }
     }
 }
 
