dc53858d7747f5b0c7d0b66a52f211073203fb44
NJuGau <nathanael.gauthama@binus.ac.id>
2024-10-04 00:25:32 +0700

    Validate image showing

--- FamilyStoryADA/FamilyStoryADAApp.swift
+++ FamilyStoryADA/FamilyStoryADAApp.swift
@@ -13,6 +13,18 @@ struct FamilyStoryADAApp: App {
         WindowGroup {
 //            ExampleView()
             CameraView()
+                .onAppear() {
+                    let fileManager = FileManager.default
+                    let url = "4ECBB088-FD5C-42CF-8C5C-5AE0D6EC5962.jpg"
+                    if let documentsDirectory = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first {
+                        let fileURL = documentsDirectory.appendingPathComponent(url)
+                        if fileManager.fileExists(atPath: fileURL.path) {
+                            print("Success: Image saved and verified at \(fileURL.path)")
+                        } else {
+                            print("Error: Image file not found at \(fileURL.path) after saving.")
+                        }
+                    }
+                }
         }
     }
 }
