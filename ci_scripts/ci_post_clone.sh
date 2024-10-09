4da6daadff3912ea6e6b4845cb615309d7cc642f
danieliank <daxter1835@gmail.com>
2024-10-09 19:57:05 +0700

    test: change hash code

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -23,15 +23,15 @@ mkdir -p FamilyStoryADA.xcodeproj/project.xcworkspace/xcshareddata/swiftpm
 echo "Creating Package.resolved..."
 cat <<EOL > FamilyStoryADA.xcodeproj/project.xcworkspace/xcshareddata/swiftpm/Package.resolved
 {
-  "originHash" : "0271592b24e5dc1185a09e3f2d980d55521827815d397c9bce8e63d502a51e84",
+  "originHash" : "13653293ccb2b03fb3a569cd04e20814001c852aa41dbb682ce436209ddd9ee2",
   "pins" : [
     {
       "identity" : "tocropviewcontroller",
       "kind" : "remoteSourceControl",
       "location" : "https://github.com/TimOliver/TOCropViewController.git",
       "state" : {
-        "revision" : "latest_revision_hash_here",
-        "version" : "2.6.0"
+        "revision" : "a634cb7cdfd580006e79a6e74e64417fe9e9783b",
+        "version" : "2.7.4"
       }
     }
   ],
