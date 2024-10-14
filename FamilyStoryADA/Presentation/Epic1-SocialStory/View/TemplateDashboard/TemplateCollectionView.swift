e1500ca96a16d2be7e2b8cfb4678c345b26a2ef7
danieliank <daxter1835@gmail.com>
2024-10-14 11:56:07 +0700

    feat: add scrollview

--- FamilyStoryADA/Presentation/Epic1-SocialStory/View/TemplateDashboard/TemplateCollectionView.swift
+++ FamilyStoryADA/Presentation/Epic1-SocialStory/View/TemplateDashboard/TemplateCollectionView.swift
@@ -23,12 +23,14 @@ struct TemplateCollectionView: View {
                         .frame(height: 756 * heightRatio)
                         .foregroundStyle(.yellow)
                         .overlay(
+                            ScrollView {
                             LazyVGrid(columns: columns, spacing: 20 * heightRatio) {
-                                ForEach(0..<6, id: \.self) { _ in
+                                ForEach(0..<20, id: \.self) { _ in
                                     TemplateCardView()
                                 }
                             }
-                                .padding(46 * widthRatio)
+                            .padding(46 * widthRatio)
+                            }
                         )
                 }
             }
