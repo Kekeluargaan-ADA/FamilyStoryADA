ec31195923ee5deef00d1a7b5784a39015fa67e8
danieliank <daxter1835@gmail.com>
2024-10-17 10:02:15 +0700

    feat: add upload photo square view connected to modal

---
+++ FamilyStoryADA/Presentation/Epic1-SocialStory/View/Customization/UploadPhotoSquareView.swift
@@ -0,0 +1,67 @@
+//
+//  UploadPhotoPage.swift
+//  FamilyStoryADA
+//
+//  Created by Daniel Ian on 17/10/24.
+//
+
+import SwiftUI
+
+struct UploadPhotoSquareView: View {
+    @State private var isModalPresented = false
+    
+    var body: some View {
+        GeometryReader { geometry in
+            let ratios = ScreenSizeHelper.calculateRatios(geometry: geometry)
+            let heightRatio = ratios.heightRatio
+            let widthRatio = ratios.widthRatio
+            
+            Button(action: {
+                isModalPresented = true
+            }) {
+                Rectangle()
+                    .foregroundColor(.clear)
+                    .frame(width: 760 * widthRatio, height: 468 * heightRatio)
+                    .cornerRadius(12)
+                    .shadow(radius: 2, y: 4)
+                    .overlay(
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 12)
+                                .inset(by: 1)
+                                .stroke(Color("FSBorderBlue7"))
+                            VStack {
+                                Image(systemName: "photo")
+                                    .foregroundStyle(Color("FSBlue9"))
+                                    .font(.system(size: 26 * heightRatio))
+                                    .bold()
+                                Spacer().frame(height: 8 * heightRatio)
+                                Text("Upload Foto")
+                                    .font(.system(size: 24 * heightRatio))
+                                    .fontWeight(.medium)
+                                    .foregroundColor(Color("FSBlue9"))
+                            }
+                        }
+                    )
+            }
+            .buttonStyle(.plain)
+            .sheet(isPresented: $isModalPresented) {
+                UploadPhotoModalView()
+            }
+        }
+    }
+}
+
+struct UploadPhotoModalView: View {
+    var body: some View {
+        VStack {
+            Text("Upload Photo Modal")
+                .font(.title)
+            // Add your modal content here
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    UploadPhotoSquareView()
+}
