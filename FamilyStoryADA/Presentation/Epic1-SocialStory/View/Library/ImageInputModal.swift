47ae7acb5aabdf8636282bb8e401a4ddcd4ec073
Vincent Junior <Vincent.halimjr@gmail.com>
2024-10-20 00:42:14 +0700

    feat: Implement ImagePicker for image preview modal

--- FamilyStoryADA/Presentation/Epic1-SocialStory/View/Library/ImageInputModal.swift
+++ FamilyStoryADA/Presentation/Epic1-SocialStory/View/Library/ImageInputModal.swift
@@ -5,32 +5,34 @@
 //  Created by Vincent Junior Halim on 14/10/24.
 //
 
-
+import PhotosUI
 import SwiftUI
 
 struct ImageInputModal: View {
-    @StateObject var viewModel = CameraViewModel()
+    @Environment(\.dismiss) var dismiss
+    @StateObject var viewModel = CameraViewModel()  // Shared ViewModel
     @Binding var isPresented: Bool
     @State private var isEditing: Bool = false
     @State private var name: String = "Hendra"
-    
+    @State private var showCropView = false  // Manage crop view state
+    @State private var selectedImage: UIImage?  // Manage selected image
+
     var body: some View {
         NavigationView {
             VStack {
                 PreviewModalHeader(isPresented: isPresented)
-                
+
                 Text("Foto ini akan digunakan pada bagian intro dan closing dari story ini.")
-                
+
                 // Display saved image if path exists, otherwise show placeholder
-                if let imagePath = viewModel.savedImageFilename,
-                   let uiImage = loadImageFromAppStorage(named: imagePath) {
+                if let uiImage = viewModel.savedImage {
                     // Display the image
                     Image(uiImage: uiImage)
                         .resizable()
                         .scaledToFit()
                         .frame(width: 300, height: 400)
                         .overlay(alignment: .bottom) {
-                            ChangePictureButton()
+                            ChangePictureButton(showCropView: $showCropView, selectedImage: $selectedImage, viewModel: viewModel)
                         }
                 } else {
                     // Placeholder if no image is found
@@ -38,10 +40,10 @@ struct ImageInputModal: View {
                         .frame(width: 300, height: 400)
                         .foregroundColor(.gray)
                         .overlay(alignment: .bottom) {
-                            ChangePictureButton()
+                            ChangePictureButton(showCropView: $showCropView, selectedImage: $selectedImage, viewModel: viewModel)
                         }
                 }
-                
+
                 HStack {
                     if isEditing {
                         // Show TextField for editing name
@@ -51,7 +53,7 @@ struct ImageInputModal: View {
                     } else {
                         Text(name)
                     }
-                    
+
                     Button(action: {
                         isEditing.toggle()  // Toggle editing state
                     }) {
@@ -60,7 +62,7 @@ struct ImageInputModal: View {
                     }
                 }
                 .padding()
-                
+
                 Button(action: {
                     // Your action for the next button
                 }) {
@@ -78,28 +80,8 @@ struct ImageInputModal: View {
         .navigationViewStyle(.stack)
         .environmentObject(viewModel)
     }
-    
-    // Function to load the image from app storage
-    func loadImageFromAppStorage(named imageName: String) -> UIImage? {
-        let fileManager = FileManager.default
-        
-        // Get the path to the app's Documents directory
-        guard let documentDirectory = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first else {
-            return nil
-        }
-        
-        // Append the image name to the directory path
-        let imagePath = documentDirectory.appendingPathComponent(imageName)
-        
-        // Check if the image file exists at the path
-        if fileManager.fileExists(atPath: imagePath.path) {
-            return UIImage(contentsOfFile: imagePath.path)
-        } else {
-            print("Image not found at path: \(imagePath.path)")
-            return nil
-        }
-    }
 }
+
 
 
 #Preview {
@@ -109,60 +91,93 @@ struct ImageInputModal: View {
 
 
 struct ChangePictureButton: View {
+    @Binding var showCropView: Bool
+    @Binding var selectedImage: UIImage?
+    @ObservedObject var viewModel: CameraViewModel  // Use ObservedObject to bind the shared viewModel
+
     @State private var navigateToCamera = false  // For taking a photo
-    @State private var navigateToPhotoPicker = false  // For choosing a photo
-    @State private var isTapped = false
+    @State private var showingImagePicker = false  // To show image picker sheet
+
     var body: some View {
-        Menu {
-                    Button(action: {
-                        // Trigger navigation to PhotoPickerView when "Choose Photo" is tapped
-                        navigateToPhotoPicker = true
-                    }) {
-                        Label("Choose Photo", systemImage: "photo")
-                    }
-                    Button(action: {
-                        // Trigger navigation to CameraView when "Take Photo" is tapped
-                        navigateToCamera = true
-                    }) {
-                        Label("Take Photo", systemImage: "camera")
-                    }
-                } label: {
-                    Text("Ubah foto")
-                        .foregroundColor(.black)
-                        .padding()
-                        .background(isTapped ? Color(.fsYellow) : Color(.fsSecondaryBlue4))  // Change background color when tapped
-                        .cornerRadius(10)
-                        .onTapGesture {
-                            isTapped = true  // Change color to indicate tap
-                            DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
-                                isTapped = false  // Reset color after a short delay
-                            }
-                        }
+        VStack {
+            Menu {
+                // Button to show the image picker sheet
+                Button(action: {
+                    showingImagePicker = true
+                }) {
+                    Label("Choose Photo", systemImage: "photo")
                 }
-                .padding(.bottom, 20)
-                
-                // NavigationLink for CameraView
-                NavigationLink(destination: CameraView()
-                    .onDisappear {
-                        // Reset the state when navigating back
-                        navigateToCamera = false
-                    }, isActive: $navigateToCamera) {
-                        EmptyView()
-                    }
 
-                // NavigationLink for PhotoPickerView
-                NavigationLink(destination: PhotoPickerViews()
-                    .onDisappear {
-                        // Reset the state when navigating back
-                        navigateToPhotoPicker = false
-                    }, isActive: $navigateToPhotoPicker) {
-                        EmptyView()
-                    }
+                // Button to trigger navigation to CameraView
+                Button(action: {
+                    navigateToCamera = true
+                }) {
+                    Label("Take Photo", systemImage: "camera")
+                }
+            } label: {
+                Text("Ubah foto")
+                    .foregroundColor(.black)
+                    .padding()
+                    .background(Color(.fsSecondaryBlue4))  // Custom background color
+                    .cornerRadius(10)
             }
-}
+            .padding(.bottom, 20)
+            
+            // NavigationLink for CameraView
+            NavigationLink(destination: CameraView(), isActive: $navigateToCamera) {
+                EmptyView()
+            }
+        }
+        .sheet(isPresented: $showingImagePicker, onDismiss: {
+            if selectedImage != nil {
+                // Show crop view once an image is selected
+                showCropView = true
+            }
+        }) {
+            ImagePicker(selectedImage: $selectedImage)
+        }
+        NavigationLink(
+            destination: cropView(),
+            isActive: $showCropView,
+            label: {
+                EmptyView()
+            }
+        )
+    }
 
-struct PhotoPickerViews: View {
-    var body: some View {
-        Text("Photo Picker View")
+    // Function to load and present CropView after image selection
+    @ViewBuilder
+    private func cropView() -> some View {
+        if let selectedImage = selectedImage {
+            CropView(image: selectedImage, croppingStyle: .default, croppingOptions: .init()) { image in
+                // Handle cropped image here
+                viewModel.capturedImage = nil
+                viewModel.photosPickerItem = nil
+
+                // Save image and get the filename
+                let filename = CameraDelegate.saveImageToAppStorage(image.image)
+                viewModel.savedImageFilename = filename
+                viewModel.savedImage = CameraDelegate.loadImageFromAppStorage(named: filename)
+                // Save the image to gallery
+                print("INI FILE NAME :\(filename)")
+                CameraDelegate.saveImageToGallery(image.image)
+
+                // Dismiss crop view after saving
+                showCropView = false
+            } didCropImageToRect: { _ in
+                // Handle additional crop rect logic if needed
+            } didFinishCancelled: { _ in
+                // Handle cancel action
+                viewModel.capturedImage = nil
+                viewModel.photosPickerItem = nil
+                showCropView = false
+            }
+            .ignoresSafeArea()
+        } else {
+            EmptyView()
+        }
     }
 }
+
+
+
