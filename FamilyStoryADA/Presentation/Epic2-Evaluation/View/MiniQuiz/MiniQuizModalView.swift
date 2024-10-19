61f581293e4c4ed97a2b62c0c281beab4d002bfe
NJuGau <nathanael.gauthama@binus.ac.id>
2024-10-19 20:25:15 +0700

    Feat: Create Mini quiz result modal view

---
+++ FamilyStoryADA/Presentation/Epic2-Evaluation/View/MiniQuiz/MiniQuizModalView.swift
@@ -0,0 +1,18 @@
+//
+//  MiniQuizModal.swift
+//  FamilyStoryADA
+//
+//  Created by Nathanael Juan Gauthama on 19/10/24.
+//
+
+import SwiftUI
+
+struct MiniQuizModalView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MiniQuizModalView()
+}
