5cd33bc7f14af557accbca2570b27420ee1e7d6b
Vincent Junior <Vincent.halimjr@gmail.com>
2024-10-20 18:51:35 +0700

    feat: add sorting and searching algorithm

--- FamilyStoryADA/Presentation/Epic1-SocialStory/View/StoryDashboard/DropdownFilterView.swift
+++ FamilyStoryADA/Presentation/Epic1-SocialStory/View/StoryDashboard/DropdownFilterView.swift
@@ -9,38 +9,39 @@ import SwiftUI
 
 struct DropdownFilterView: View {
     
+    @ObservedObject var viewModel: StoryViewModel
     @Binding var selectedOption: SortOption
-
-        var body: some View {
-            Menu {
-                Button(action: {
-                    selectedOption = SortOption.newest
-                }) {
-                    Text(SortOption.newest.rawValue)
-                }
-                Button(action: {
-                    print("Oldest")
-                    selectedOption = SortOption.oldest
-                    print(selectedOption)
-                }) {
-                    Text(SortOption.oldest.rawValue)
-                }
-            } label: {
-                HStack {
-                    Text(selectedOption.rawValue)
-                        .font(.system(size: 16, weight: .medium))
-                        .foregroundStyle(Color("FSBlack"))
-                    Image(systemName: "chevron.down")
-                        .font(.system(size: 12, weight: .medium))
-                }
-                .padding()
-                .background(Color("FSBlue1"))
-                .frame(width: 117, height: 28)
-                .cornerRadius(20)
+    
+    var body: some View {
+        Menu {
+            Button(action: {
+                selectedOption = .newest
+                viewModel.selectedOption = .newest
+                viewModel.sortDisplayedStories()  // Trigger sorting in the ViewModel
+            }) {
+                Text(SortOption.newest.rawValue)
             }
+            Button(action: {
+                
+                selectedOption = .oldest
+                viewModel.selectedOption = .oldest
+                viewModel.sortDisplayedStories()  // Trigger sorting in the ViewModel
+            }) {
+                Text(SortOption.oldest.rawValue)
+            }
+        } label: {
+            HStack {
+                Text(selectedOption.rawValue)
+                    .font(.system(size: 16, weight: .medium))
+                    .foregroundStyle(Color("FSBlack"))
+                Image(systemName: "chevron.down")
+                    .font(.system(size: 12, weight: .medium))
+            }
+            .padding()
+            .background(Color("FSBlue1"))
+            .frame(width: 117, height: 28)
+            .cornerRadius(20)
         }
+    }
 }
 
-#Preview {
-    DropdownFilterView(selectedOption: .constant(.oldest))
-}
