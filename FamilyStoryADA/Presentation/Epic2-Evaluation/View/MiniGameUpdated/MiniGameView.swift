316454bd6b01b1df9004a80082fe5bd72236819b
NJuGau <nathanael.gauthama@binus.ac.id>
2024-11-06 09:46:09 +0700

    Feat: Make class for mini game view

---
+++ FamilyStoryADA/Presentation/Epic2-Evaluation/View/MiniGameUpdated/MiniGameView.swift
@@ -0,0 +1,18 @@
+//
+//  MiniGameView.swift
+//  FamilyStoryADA
+//
+//  Created by Nathanael Juan Gauthama on 06/11/24.
+//
+
+import SwiftUI
+
+struct MiniGameView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MiniGameView()
+}
---
+++ FamilyStoryADA/Presentation/Epic2-Evaluation/ViewModel/MiniGameViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  MiniGameViewModel.swift
+//  FamilyStoryADA
+//
+//  Created by Nathanael Juan Gauthama on 06/11/24.
+//
+
+import Foundation
+
+class MiniGameViewModel: Imageable, ObservableObject {
+    
+}
