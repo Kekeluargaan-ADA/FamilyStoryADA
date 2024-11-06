ac4e8ecc650f821bb4133155e1e0e6c4ac4bef9d
NJuGau <nathanael.gauthama@binus.ac.id>
2024-11-06 11:25:08 +0700

    Feat: Add option card view

--- FamilyStoryADA/Presentation/Epic2-Evaluation/View/MiniGameUpdated/MiniGamOptionCardView.swift
+++ FamilyStoryADA/Presentation/Epic2-Evaluation/View/MiniGameUpdated/MiniGamOptionCardView.swift
@@ -8,11 +8,36 @@
 import SwiftUI
 
 struct MiniGamOptionCardView: View {
+    var image: UIImage?
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        if let displayedImage = image {
+            ZStack {
+                Image(uiImage: displayedImage)
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 340, height: 191)
+                    .clipShape(
+                        RoundedRectangle(cornerRadius: 16)
+                    )
+                
+                RoundedRectangle(cornerRadius: 16)
+                    .stroke(style: StrokeStyle(lineWidth: 5))
+                    .frame(width: 340, height: 191)
+                    .foregroundStyle(Color("FSWhite"))
+            }
+        } else {
+            RoundedRectangle(cornerRadius: 16)
+                .frame(width: 340, height: 191)
+                .foregroundStyle(.clear)
+        }
     }
 }
 
 #Preview {
-    MiniGamOptionCardView()
+    ZStack {
+        Color(.red)
+        MiniGamOptionCardView(image: UIImage(imageLiteralResourceName: "MenggosokGigiScene1"))
+        
+    }
 }
