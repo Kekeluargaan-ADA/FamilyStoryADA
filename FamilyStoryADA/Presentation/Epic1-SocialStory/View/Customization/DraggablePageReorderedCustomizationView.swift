d051b4cad11ee6bb509ee8304cecd2972aa1b70e
NJuGau <nathanael.gauthama@binus.ac.id>
2024-11-11 10:52:43 +0700

    Fix: Fix customization draggable page background color

--- FamilyStoryADA/Presentation/Epic1-SocialStory/View/Customization/DraggablePageReorderedCustomizationView.swift
+++ FamilyStoryADA/Presentation/Epic1-SocialStory/View/Customization/DraggablePageReorderedCustomizationView.swift
@@ -13,80 +13,78 @@ struct DraggablePageReorderedCustomizationView: View {
     @Binding var introPages: [DraggablePage]
     
     var body: some View {
-        ZStack {
-            Color("FSBlue6")
-                .ignoresSafeArea()
-            List {
-                ForEach(introPages, id: \.id) { page in
-                    if let image = viewModel.loadImageFromDiskWith(fileName: page.picturePath) {
-                        DraggedPageView(imagePath: image,
-                                        order: page.order+1,
-                                        isSelected: viewModel.selectedPage?.pageId == page.id
-                        )
-                        .onTapGesture {
-                            viewModel.selectPage(page: page)
-                        }
-                    } else if page.picturePath != "" {
-                        DraggedPageView(imagePath: UIImage(imageLiteralResourceName: page.picturePath),
-                                        order: page.order+1,
-                                        isSelected: viewModel.selectedPage?.pageId == page.id
-                        )
-                        .onTapGesture {
-                            viewModel.selectPage(page: page)
-                        }
-                    } else {
-                        DraggedPageView(order: page.order+1,
-                                        isSelected: viewModel.selectedPage?.pageId == page.id
-                        )
-                        .onTapGesture {
-                            viewModel.selectPage(page: page)
-                        }
+        List {
+            ForEach(introPages, id: \.id) { page in
+                if let image = viewModel.loadImageFromDiskWith(fileName: page.picturePath) {
+                    DraggedPageView(imagePath: image,
+                                    order: page.order+1,
+                                    isSelected: viewModel.selectedPage?.pageId == page.id
+                    )
+                    .onTapGesture {
+                        viewModel.selectPage(page: page)
                     }
-                }
-                
-                ForEach(draggedPages, id: \.id) { page in
-                    if let image = viewModel.loadImageFromDiskWith(fileName: page.picturePath) {
-                        DraggedPageView(imagePath: image,
-                                        order: page.order + introPages.count + 1,
-                                        isSelected: viewModel.selectedPage?.pageId == page.id
-                        )
-                        .onTapGesture {
-                            viewModel.selectPage(page: page)
-                        }
-                    } else if page.picturePath != "" {
-                        DraggedPageView(imagePath: UIImage(imageLiteralResourceName: page.picturePath),
-                                        order: page.order + introPages.count + 1,
-                                        isSelected: viewModel.selectedPage?.pageId == page.id
-                        )
-                        .onTapGesture {
-                            viewModel.selectPage(page: page)
-                        }
-                    } else {
-                        DraggedPageView(order: page.order  + introPages.count + 1,
-                                        isSelected: viewModel.selectedPage?.pageId == page.id
-                        )
-                        .onTapGesture {
-                            viewModel.selectPage(page: page)
-                        }
+                } else if page.picturePath != "" {
+                    DraggedPageView(imagePath: UIImage(imageLiteralResourceName: page.picturePath),
+                                    order: page.order+1,
+                                    isSelected: viewModel.selectedPage?.pageId == page.id
+                    )
+                    .onTapGesture {
+                        viewModel.selectPage(page: page)
                     }
-                }
-                .onMove { indexSet, destination in
-                    draggedPages.move(fromOffsets: indexSet, toOffset: destination)
-                    viewModel.reorderPage()
-                }
-                
-                // add button only appear if visible page is below 10
-                if  viewModel.isAddButtonAppeared() {
-                    Button(action: {
-                        viewModel.addNewBlankPage()
-                    }, label: {
-                        AddNewPageButtonView()
-                    })
+                } else {
+                    DraggedPageView(order: page.order+1,
+                                    isSelected: viewModel.selectedPage?.pageId == page.id
+                    )
+                    .onTapGesture {
+                        viewModel.selectPage(page: page)
+                    }
                 }
             }
-            .listRowBackground(Color.clear)
-            .background(Color.clear)
+            
+            ForEach(draggedPages, id: \.id) { page in
+                if let image = viewModel.loadImageFromDiskWith(fileName: page.picturePath) {
+                    DraggedPageView(imagePath: image,
+                                    order: page.order + introPages.count + 1,
+                                    isSelected: viewModel.selectedPage?.pageId == page.id
+                    )
+                    .onTapGesture {
+                        viewModel.selectPage(page: page)
+                    }
+                } else if page.picturePath != "" {
+                    DraggedPageView(imagePath: UIImage(imageLiteralResourceName: page.picturePath),
+                                    order: page.order + introPages.count + 1,
+                                    isSelected: viewModel.selectedPage?.pageId == page.id
+                    )
+                    .onTapGesture {
+                        viewModel.selectPage(page: page)
+                    }
+                } else {
+                    DraggedPageView(order: page.order  + introPages.count + 1,
+                                    isSelected: viewModel.selectedPage?.pageId == page.id
+                    )
+                    .onTapGesture {
+                        viewModel.selectPage(page: page)
+                    }
+                }
+            }
+            .onMove { indexSet, destination in
+                draggedPages.move(fromOffsets: indexSet, toOffset: destination)
+                viewModel.reorderPage()
+            }
+            
+            // add button only appear if visible page is below 10
+            if  viewModel.isAddButtonAppeared() {
+                Button(action: {
+                    viewModel.addNewBlankPage()
+                }, label: {
+                    AddNewPageButtonView()
+                })
+                .listRowBackground(Color.clear)
+                .listRowSeparator(.hidden)
+            }
         }
+        .background(Color.clear)
+        .scrollContentBackground(.hidden)
     }
 }
 
