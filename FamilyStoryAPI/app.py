29cc2df57bdeaf68c05d511680c04e6b665ed8b7
Vincent Junior <Vincent.halimjr@gmail.com>
2024-11-16 20:16:11 +0700

    feat: add image generator and improve paraphrasing prompt

--- FamilyStoryAPI/app.py
+++ FamilyStoryAPI/app.py
@@ -9,6 +9,19 @@ import re
 import uuid
 import hashlib
 import asyncio
+import requests
+import time
+import io
+from PIL import Image
+import base64
+from googletrans import Translator
+
+
+model_id = "stabilityai/stable-diffusion-3.5-large-turbo"
+API_URL = f"https://api-inference.huggingface.co/models/{model_id}"
+headers = {"Authorization": "Bearer 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"}
+
+
 
 nest_asyncio.apply()
 
@@ -20,6 +33,52 @@ if not os.path.exists('images'):
 app.mount("/images", StaticFiles(directory="images"), name="images")
 
 all_urls = []
+
+def query(payload):
+    start_time = time.time()
+    response = requests.post(API_URL, headers=headers, json=payload)
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+
+    print(f"Elapsed time: {elapsed_time:.2f} seconds")
+    return response.content
+
+async def generateImage(keyword: str,user_id):
+    translator = Translator()
+
+    # Text to translate (Indonesian to English)
+
+    # Translate text
+    translated = translator.translate(keyword, src='id', dest='en')
+    
+    keyword = translated.text
+    
+    prompt = f"""
+Generate a simple, clean image inspired by {keyword}, focusing on childhood. Use soft colors, minimal details, and playful shapes to convey innocence and joy in a straightforward, uncluttered style.
+"""
+    output = query({
+        "inputs": prompt
+    })
+    if isinstance(output, bytes):
+        image = Image.open(io.BytesIO(output))
+        if image.mode != "RGB":
+            image = image.convert("RGB")
+        file_path = os.path.join("images",user_id, "000001.jpg")
+        image.save(file_path, format="JPEG")
+        print(f"Image saved at: {file_path}")
+    elif isinstance(output, str):
+        try:
+            image_data = base64.b64decode(output)
+            image = Image.open(io.BytesIO(image_data))
+            if image.mode != "RGB":
+                image = image.convert("RGB")
+            file_path = os.path.join("images",user_id, "000001.jpg")
+            image.save(file_path, format="JPEG")
+            print(f"Image saved at: {file_path}")
+        except:
+            print("Failed to decode the string as a base64 encoded image.")
+    else:
+        print("Output is not in a recognizable format (bytes or base64 encoded string).")
 
 def checkCrawlURL(log_record):
     """Capture image URLs from log messages and store them in all_urls."""
@@ -46,7 +105,7 @@ async def async_download_images(keyword: str, max_num: int = 3, user_id: str = N
 
     filters = dict(license='commercial,modify')
     
-    await asyncio.to_thread(google_crawler.crawl, keyword=keyword, filters=filters, max_num=max_num, file_idx_offset=0)
+    await asyncio.to_thread(google_crawler.crawl, keyword=keyword, filters=filters, max_num=max_num, file_idx_offset=2)
     
     image_files = [os.path.join(user_dir, f) for f in os.listdir(user_dir) if f.endswith((".jpg", ".png", ".jpeg"))]
 
@@ -64,10 +123,12 @@ async def crawl_images(
 ):
     global all_urls
     all_urls.clear()
-
-    # Pass user_id to the async_download_images function
-    await async_download_images(keyword, max_num, user_id)
-
+    
+    tasks = [
+    asyncio.create_task(async_download_images(keyword, max_num, user_id)),
+    asyncio.create_task(generateImage(keyword=keyword, user_id=user_id))
+]
+    results = await asyncio.gather(*tasks)
     folder_path = os.path.join("images", user_id)
     
     # Check if the user's folder exists
